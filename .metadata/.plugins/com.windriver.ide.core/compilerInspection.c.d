d2af849ab0668409b6ddae80deefc4e90f670aa3
b-mck <denali.99755+github@gmail.com>
2014-04-18 15:46:24 -0700

    Removed WindRiver .metadata folder

--- .metadata/.plugins/com.windriver.ide.core/compilerInspection.c.d
+++
@@ -1,2 +0,0 @@
-compilerInspection.o:  \
- C:/WindRiver/workspace/.metadata/.plugins/com.windriver.ide.core/compilerInspection.c
--- .metadata/.plugins/com.windriver.ide.core/compilerInspection.cpp.d
+++
@@ -1,2 +0,0 @@
-compilerInspection.o:  \
- C:/Users/s.wells/Documents/GitHub/tl-frc/.metadata/.plugins/com.windriver.ide.core/compilerInspection.cpp
